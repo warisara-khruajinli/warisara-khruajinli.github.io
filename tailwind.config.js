6ad9cfdcb80c3a095e1aad0d91e4c4bc892a8aa8
warisara <warisara.khrua@mail.kmutt.ac.th>
2023-03-27 01:30:33 +0700

    version2

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html",
+    "./decoder.html",
+    "./press.html",
+    "./ramakien.html",
+    "./blocky.html",
+    "./thinkdq.html",
+    "./punmaew.html",
+    "./sena.html",
+    "./prompkai.html",
+    "./wipcamp12.html",
+    "./helloDolphine.html",
+    "./helloThailand.html",
+    "./sitCareer.html",
+    "./uxProject.html",
+    "./integrated.html",
+    "./lowcode.html",
+    "./about.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
